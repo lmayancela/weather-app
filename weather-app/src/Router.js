c534eadfb41ec9740109ae2fa9d04cfa63c8a4f9
Lisandro Mayancela <lmayancela08@gmail.com>
2018-02-12 22:19:07 +0000

    First Commit

---
+++ weather-app/src/Router.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom';
+
+const BasicExample = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Weekly Forecast</Link></li>
+        <li><Link to="/day">Day</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Weekly}/>
+      <Route path="/day" component={Day}/>
+    </div>
+  </Router>
+);
+
+const Weekly = () => (
+  <div>
+    <h2>Weekly Forecast</h2>
+    <ul>
+      <li> Monday </li>
+    </ul>
+  </div>
+);
+
+const Day = () => (
+  <div>
+    <h2>Daily Forecast</h2>
+    <p>It will be cloudy on Monday</p>
+  </div>
+);
+
+
+
+export default BasicExample;
